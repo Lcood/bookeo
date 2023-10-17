5fa5bc861d5128b1a581d8fd5ffa1c5051918627
Lcood <fatoumatadiallo@MBPdeFatoumata.lan>
2023-10-17 12:36:20 -0700

    initial page

---
+++ templates/home.php
@@ -0,0 +1,53 @@
+<div class="container py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="./assets/images/logo-bookeo.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="350" >
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  <!--cards-->
+   <div class="container">
+        <div class="row">
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                            <img src="./uploads/books/1-1984.jpg" class="card-img-top" alt="1984">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                </div>
+    
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                            <img src="./uploads/books/2-histoires-courtes.jpg" class="card-img-top" alt="histoire courte">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                </div>
+        
+                <div class="col-md-4 my-2">
+                    <div class="card">
+                            <img src="./uploads/books/3-zai-zai-zai-zai.jpg" class="card-img-top" alt="Zai zai">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                </div>
+        </div>
+    </div>
